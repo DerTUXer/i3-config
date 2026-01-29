bd63459730c2b47c3631f14ef6866d3648a6433d
Der TUXer <127011473+DerTUXer@users.noreply.github.com>
2026-01-29 19:38:25 +0100

    Update package installations in 1-software.sh

--- 1-software.sh
+++ 1-software.sh
@@ -8,7 +8,7 @@ sudo nala update
 sudo nala install -y xorg xbacklight xinput xorg-dev xdotool brightnessctl
 sudo nala install -y x11-xserver-utils
 sudo nala install -y python3-pip
-sudo nala install -y npm
+sudo nala install -y npm zig
 sudo nala install -y default-jdk
 
 # CPU Microcode Updates (if you have amd cpu uncomment the first one and comment the second one)
@@ -76,13 +76,13 @@ sudo nala install -y zathura qiv
 sudo nala install -y gnome-disk-utility bleachbit
 
 # Neovim Build prerequisites
-sudo nala install -y ninja-build gettext cmake curl python3.11-venv
+sudo nala install -y ninja-build gettext cmake curl python3-venv
 
 # Required packages for i3-gaps installation
 sudo nala install -y meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev 
 sudo nala install -y libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev 
 sudo nala install -y libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 
-sudo nala install -y libxcb-shape0-dev i3status i3lock cmake
+sudo nala install -y i3-wm libxcb-shape0-dev i3status i3lock cmake i3lock-fancy
 
 # Brave-Browser
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
